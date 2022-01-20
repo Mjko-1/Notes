fcc9776ff6365a33efec1ee99752e7d608df220c
Mjko-1 <evdokimoff.vlad2011@yandex.ru>
2022-01-20 09:39:23 +0300

    Сохранение состояния WebView при перевороте экрана и вывод Toast при отсутствии разрешения на геолокацию

--- app/src/main/java/com/example/notes/ui/about/LocationFragment.kt
+++ app/src/main/java/com/example/notes/ui/about/LocationFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.example.notes.R
 import com.example.notes.databinding.FragmentLocationBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -51,9 +52,17 @@ class LocationFragment : Fragment() {
         ) {
             ActivityCompat.requestPermissions(
                 requireActivity(),
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
                 101
             )
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.permission_required),
+                Toast.LENGTH_LONG
+            ).show()
             return
         }
         task.addOnSuccessListener {

82dac1f8fcfa0e5528afc1884f21482186eb790b
Mjko-1 <evdokimoff.vlad2011@yandex.ru>
2022-01-24 09:07:45 +0300

    Добавление кнопки перехода к настройкам, если пользователь запретил дальнейшее напоминание о разрешении

--- app/src/main/java/com/example/notes/ui/about/location/LocationFragment.kt
+++ app/src/main/java/com/example/notes/ui/about/location/LocationFragment.kt
@@ -1,15 +1,22 @@
 package com.example.notes.ui.about.location
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.notes.BuildConfig
+import com.example.notes.R
 import com.example.notes.databinding.FragmentLocationBinding
 import com.example.notes.ui.dialogs.LocationAgreePermissionDialog
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -24,6 +31,11 @@ class LocationFragment : Fragment() {
     private val viewModel by lazy { ViewModelProvider(this)[LocationViewModel::class.java] }
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private val requestLocationPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions(),
+        ::onGotLocationPermissionResult
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,35 +53,45 @@ class LocationFragment : Fragment() {
         setupOnClickListeners()
     }
 
+    override fun onResume() {
+        super.onResume()
+        if (checkPermissions()) binding.buttonSettings.isVisible = false
+    }
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
 
-    private fun fetchLocation() {
-        when {
-            checkLocationPermissions() -> {
-                viewModel.requestLocationCoordinates(fusedLocationProviderClient)
-            }
-            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
-                LocationAgreePermissionDialog().show(parentFragmentManager, "Agree dialog")
-            }
-            else -> {
-                ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    arrayOf(
-                        Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ),
-                    LOCATION_REQUEST_CODE
-                )
+    private fun onGotLocationPermissionResult(grantResults: Map<String, Boolean>) {
+        if (grantResults.entries.all { it.value }) {
+            viewModel.requestLocationCoordinates(fusedLocationProviderClient)
+        } else {
+            if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                binding.buttonSettings.isVisible = true
+                showToast(getString(R.string.permission_denied))
+            } else {
+                LocationAgreePermissionDialog().show(parentFragmentManager, "Agree_dialog")
             }
         }
     }
 
     private fun setupOnClickListeners() {
         binding.buttonLocation.setOnClickListener {
-            fetchLocation()
+            requestLocationPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+        binding.buttonSettings.setOnClickListener {
+            startActivity(
+                Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                )
+            )
         }
     }
 
@@ -79,7 +101,11 @@ class LocationFragment : Fragment() {
         }
     }
 
-    private fun checkLocationPermissions(): Boolean = ActivityCompat.checkSelfPermission(
+    private fun showToast(text: String) {
+        Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun checkPermissions(): Boolean = ActivityCompat.checkSelfPermission(
         requireContext(),
         Manifest.permission.ACCESS_FINE_LOCATION
     ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
@@ -87,13 +113,7 @@ class LocationFragment : Fragment() {
         Manifest.permission.ACCESS_COARSE_LOCATION
     ) == PackageManager.PERMISSION_GRANTED
 
-    private fun showToast(text: String) {
-        Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
-    }
-
     companion object {
-
-        private const val LOCATION_REQUEST_CODE = 101
 
         @JvmStatic
         fun newInstance() = LocationFragment()
--- app/src/main/java/com/example/notes/ui/dialogs/LocationAgreePermissionDialog.kt
+++ app/src/main/java/com/example/notes/ui/dialogs/LocationAgreePermissionDialog.kt
@@ -1,32 +1,28 @@
 package com.example.notes.ui.dialogs
 
+import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.DialogFragment
-import com.example.notes.BuildConfig
 import com.example.notes.R
 
 class LocationAgreePermissionDialog : DialogFragment() {
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             AlertDialog.Builder(it).apply {
-                setTitle(R.string.location_agree_dialog_title)
                 setMessage(R.string.location_agree_dialog_message)
                 setNegativeButton(R.string.negative_button_text, null)
                 setPositiveButton(R.string.positive_button_text) { _, _ ->
-                    startActivity(
-                        Intent(
-                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                            Uri.fromParts(
-                                "package",
-                                BuildConfig.APPLICATION_ID,
-                                null
-                            )
-                        )
+                    ActivityCompat.requestPermissions(
+                        requireActivity(),
+                        arrayOf(
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        ),
+                        101
                     )
                 }
             }.create()
